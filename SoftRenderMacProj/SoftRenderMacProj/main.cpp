1f889100f8e129e9adca46ce95e941d8b43548a8
zhangc <zhangc@shanggame.com>
2016-07-15 17:26:36 +0800

    add mac project

---
+++ SoftRenderMacProj/SoftRenderMacProj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SoftRenderMacProj
+//
+//  Created by chao on 16/7/15.
+//  Copyright © 2016年 chao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
